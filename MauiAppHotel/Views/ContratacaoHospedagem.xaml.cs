632e2b3d5e2b78f3ce766ed68a3488df8e43cdb3
GabSampaiooo <gab.sampaio.gs@hotmail.com>
2024-11-07 18:13:45 -0300

    Add recursos ao App

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,21 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Sobre());
+    }
+
+    
+}
