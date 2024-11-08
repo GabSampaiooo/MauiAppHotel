07f452a190790354076cdcd813be985d87f02c44
GabSampaiooo <gab.sampaio.gs@hotmail.com>
2024-11-08 12:45:16 -0300

    Adição de botao para a página inicial na tela das informações de hospedagem

--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -18,4 +18,17 @@ public partial class HospedagemContratada : ContentPage
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
     }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            await Navigation.PushAsync(new MainPage());
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
